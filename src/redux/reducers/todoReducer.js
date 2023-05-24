d12059bbc21b995c05e73952c20d27e1f85dd1b3
dadereindraf <dadereindraf@gmail.com>
2023-05-24 12:56:53 +0700

    Menghapus baris kode action

--- src/redux/reducers/todoReducer.js
+++ src/redux/reducers/todoReducer.js
@@ -1,33 +1,7 @@
+import { ADD_TODO, DELETE_TODO, UPDATE_TODO } from '../actions/todoAction';
+
 const initialState = {
   todos: [],
-};
-
-const ADD_TODO = "ADD_TODO";
-const DELETE_TODO = "DELETE_TODO";
-const UPDATE_TODO = "UPDATE_TODO";
-
-const addTodo = (newTodo) => {
-  return {
-    type: ADD_TODO,
-    payload: newTodo,
-  };
-};
-
-const deleteTodo = (id) => {
-  return {
-    type: DELETE_TODO,
-    payload: id,
-  };
-};
-
-const updateTodo = (id, updatedTodo) => {
-  return {
-    type: UPDATE_TODO,
-    payload: {
-      id,
-      updatedTodo,
-    },
-  };
 };
 
 const todoReducer = (state = initialState, action) => {
@@ -55,5 +29,4 @@ const todoReducer = (state = initialState, action) => {
   }
 };
 
-export { addTodo, deleteTodo, updateTodo };
 export default todoReducer;
